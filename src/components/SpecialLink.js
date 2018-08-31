d7dd4cf9267e00dea8a9e95b69242f43f050eb15
theschmocker <jacobcs1996@gmail.com>
2018-08-31 14:51:03 -0500

    default 'to' prop to /

--- src/components/SpecialLink.js
+++ src/components/SpecialLink.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+// TODO: use proptypes and set a default for to
 const SpecialLink = ({ children, to }) => (
-    <Link 
-        to={to} 
-        className="special"
-    >
-        {children}
-    </Link>
-)
+  <Link to={to || '/'} className="special">
+    {children}
+  </Link>
+);
 
 export default SpecialLink;

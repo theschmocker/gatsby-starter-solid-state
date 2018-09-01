379708e49d14b6ae971c4290a06748ed793d6bd2
theschmocker <jacobcs1996@gmail.com>
2018-09-01 13:06:53 -0500

    Add siteTitle to Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-const Banner = React.forwardRef((props, ref) => (
+const Banner = React.forwardRef(({ siteTitle }, ref) => (
   <section ref={ref} id="banner">
     <div className="inner">
       <div className="logo">
         <span className="icon fa-diamond" />
       </div>
-      <h2>This is Solid State</h2>
+      <h2>This is {siteTitle}</h2>
       <p>
-        Another free + fully responsive site template by{' '}
+        Based upon the free + fully responsive site template by {' '}
         <a href="http://html5up.net">HTML5 UP</a>
       </p>
     </div>

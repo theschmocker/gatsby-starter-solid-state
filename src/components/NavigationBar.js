0095322808884812c6058ca4902d9867f4749099
theschmocker <jacobcs1996@gmail.com>
2018-09-01 10:21:58 -0500

    rename header to NavigationBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,11 +3,11 @@ import React from 'react';
 import SiteTitle from './SiteTitle';
 import MenuButton from './MenuButton';
 
-const Header = ({ siteTitle, openMenu, className }) => (
+const NavigationBar = ({ siteTitle, openMenu, className }) => (
   <header id="header" className={className}>
     <SiteTitle>{siteTitle}</SiteTitle>
     <MenuButton onClick={openMenu} />
   </header>
 );
 
-export default Header;
+export default NavigationBar;

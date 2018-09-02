e77165f069875bfbf458301cd21a9c1254d3f2a0
theschmocker <jacobcs1996@gmail.com>
2018-09-02 16:31:49 -0500

    configure manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,21 @@ module.exports = {
       options: {
         name: 'images',
         path: `${__dirname}/src/images`,
-      }
+      },
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gatsty Starter Solid State',
+        short_name: 'Solid State',
+        start_url: '/',
+        background_color: '#2e3141',
+        theme_color: '#4c5c96',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png',
+      },
+    },
   ],
-}
+};
